226a111d9eb06a1d50f308a852a28ec312d9894d
futpib <futpib@gmail.com>
2017-02-27 22:39:51 +0300

    Rework the transform

--- index.js
+++ index.js
@@ -1,9 +1,88 @@
-module.exports = function () {
+
+const template = require('babel-template');
+
+const defineTemplate = template(`
+	di.define(MODULE_NAME, [SOURCES], function (PARAMS) {
+		const module = { exports: {} };
+		const exports = module.exports;
+
+		BODY;
+
+		return module.exports;
+	});
+`);
+
+module.exports = function (babel) {
+	const {types: t} = babel;
+
+	function isRequireCall(path) {
+		if (!path.isCallExpression()) {
+			return false;
+		}
+		if (!path.get('callee').isIdentifier({name: 'require'})) {
+			return false;
+		}
+		if (path.scope.getBinding('require')) {
+			return false;
+		}
+
+		return true;
+	}
+
+	const nekoVisitor = {
+		VariableDeclarator(path) {
+			const id = path.get('id');
+			if (!id.isIdentifier()) {
+				return;
+			}
+
+			const init = path.get('init');
+			if (!isRequireCall(init)) {
+				return;
+			}
+
+			const source = init.node.arguments[0];
+			this.sources.push([id.node, source]);
+
+			path.remove();
+		}
+	};
+
 	return {
-		inherits: require('babel-plugin-transform-es2015-modules-amd'),
+		inherits: require('babel-plugin-transform-es2015-modules-commonjs'),
+
 		pre() {
-			if (!this.getModuleName()) {
-				throw new Error('`moduleId` is required');
+			this.sources = [];
+
+			this.moduleName = this.getModuleName();
+
+			if (!this.moduleName) {
+				throw new Error('`moduleName` is required (check babel `moduleId` options)');
+			}
+		},
+
+		visitor: {
+			Program: {
+				exit(path) {
+					if (this.ran) {
+						return;
+					}
+					this.ran = true;
+
+					path.traverse(nekoVisitor, this);
+
+					const params = this.sources.map(source => source[0]);
+					const sources = this.sources.map(source => source[1]);
+
+					const moduleName = t.stringLiteral(this.moduleName);
+
+					path.node.body = [defineTemplate({
+						MODULE_NAME: moduleName,
+						SOURCES: sources,
+						PARAMS: params,
+						BODY: path.node.body
+					})];
+				}
 			}
 		}
 	};
--- test/index.js
+++ test/index.js
@@ -5,11 +5,11 @@ const babel = require('babel-core');
 
 test('works', t => {
 	const actual = babel.transform(`
-		import defaultMember from "module-name";
+		import defaultMember from 'dependency-module-id';
 		export default 42;
 	`, {
 		plugins: ['.'],
-		moduleId: 'module-id'
+		moduleId: 'defined-module-id'
 	}).code;
 	t.snapshot(actual); // eslint-disable-line ava/use-t-well
 });
